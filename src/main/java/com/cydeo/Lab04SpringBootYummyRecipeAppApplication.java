1dfce49836876189710de4f47d67ae26bb09d259
Umit Bulut <umitbulut1993@hotmail.com>
2024-05-22 20:04:54 -0400

    App Design has launched.

---
+++ src/main/java/com/cydeo/Lab04SpringBootYummyRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04SpringBootYummyRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab04SpringBootYummyRecipeAppApplication.class, args);
+    }
+
+}
